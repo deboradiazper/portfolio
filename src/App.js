0e994b8bdcac425738a555064a034fc2345f694f
deboradiazper <deboradiazper@gmail.comn>
2022-10-19 22:13:48 +0200

    probando tsparticles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { Route, Routes, Link } from "react-router-dom";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 import { Home } from "./Pages/Home";
 import { About } from "./Pages/About";
 import { NotFound } from "./Pages/NotFound";
@@ -6,6 +10,21 @@ import { Contact } from "./Pages/Contact";
 
 
 function App() {
+  
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+  
+  
+  
   return (
   <>
   <nav>
@@ -13,15 +32,97 @@ function App() {
       <p>
         <Link to="/">Home</Link>
       </p>
-      <p>
+      {/* <p>
         <Link to="/about">about</Link>
       </p>
       <p>
         <Link to="/contact">contact</Link>
-      </p>
+      </p> */}
   </ul>
-    
   </nav>
+
+  
+  <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#0d47a1",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+
+
+
+
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/about" element={<About />}/>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,25 +1,36 @@
 import React from "react";
+import './Home.css';
+ 
 
 export const Home = () => {
     return (
         <div>
-            <div class="container">
+            <div class="container home">
                 <div class="row">
                     <div class="col-md-8"></div>
-                    <div class="col-6 col-md-4">Technologies</div>
+                    <div class="col-6 col-md-4">
+                        <button className="button">
+                            <span> Technologies
+                            </span>
+                        </button>
+                    </div>
                 </div>
 
                 <div class="row">
-                    <div class="col-6 col-md-4">About</div>
                     <div class="col-6 col-md-4">
-                        <h1 className="mainTitle">hello, I'm Débora,</h1>
-                        <h5 className="subTitle">web developer & designer</h5>
+                        About
+                    </div>
+                    <div class="col-6 col-md-4">
+                        <h1 className="mainTitle">Hello I'm Débora.</h1>
+                        <h5 className="subTitle">I'm a web developer who loves code, design and build things</h5>
                     </div>
                     <div class="col-6 col-md-4"></div>
                 </div>
 
                 <div class="row">
-                    <div class="col-md-9">Contact</div>
+                    <div class="col-md-9">
+                        Contact
+                    </div>
                     <div class="col-md-3">Projects</div>
                 </div>
             </div>
