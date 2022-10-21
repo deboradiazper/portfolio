ba2cf289f90e6ca5fddf7e92c04cae46bd3cce02
deboradiazper <deboradiazper@gmail.comn>
2022-10-21 13:08:19 +0200

    popup contacts

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -3,9 +3,11 @@ import emailjs from '@emailjs/browser';
 import './Contact.css';
 
 export const Contact = () => {
-    const form = useRef();
+  
+  const form = useRef(null);
 
-  const sendEmail = (e) => {
+  //email js template/presets + reset input
+  const sendEmail=(e)=> {
     e.preventDefault();
 
     emailjs.sendForm('service_zkai9zg', 'template_vs5bb3u', form.current, '4r9FdI6Jp62p5f0Hm')
@@ -14,27 +16,42 @@ export const Contact = () => {
       }, (error) => {
           console.log(error.text);
       });
+
+    e.target.reset();
   };
 
+  
+  
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+
+    //form (uncontrolled)
+    <form ref={form} onSubmit={sendEmail} className="rowForm flex-column text-center">
+
+      <div className="Name col-auto">
+        <label>Name</label>
+        </div>
+        <div className="Name col-auto">
+        <input type="text" name="user_name" />
+      </div>
+
+      <div className="email col-auto">
+        <label>Email</label>
+      </div>
+      <div className="email col-auto">
+        <input type="email" name="user_email" />
+      </div>
+
+      <div className="message col-auto">
+        <label>Message</label>
+      </div>
+      <div className="email col-auto">
+        <textarea name="message" />
+      </div>
+
+        <div className="submit col-auto">
+        <button className="button" type="submit" value="Send">send</button>
+      </div>
+
     </form>
 );
-}
-
-
-
-{/* <label>Name</label>
-<input type="text" name="user_name" />
-<label>Email</label>
-<input type="email" name="user_email" />
-<label>Message</label>
-<textarea name="message" />
-<input type="submit" value="Send" /> */}
+  }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import './Home.css';
- 
+import { Contact } from "./Contact";
+
 
 export const Home = () => {
+
+const [popUp, setPop] = useState(false)
+
+const handleClickOpen=()=> {
+    setPop(!popUp)
+}
+
+//maybe no hace falta al final, al hacer click en el boton ya desaparece
+const closePopUp=()=>{
+    setPop(false)
+}
+
     return (
         
         <div>
-            
-            <div class="container home">
+            <div class="containerCenter">
+
                 <div class="row">
                     <div class="col-md-8"></div>
                     <div class="col-6 col-md-4">
                         <button className="button">
-                            <span> Technologies
-                            </span>
+                            Technologies
                         </button>
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-6 col-md-4">
-                    <button className="button">
-                            <span> About
-                            </span>
+                        <button className="button">
+                                About
                         </button>
                     </div>
-                    <div class="col-6 col-md-4">
+
+                    <div className="col-6 col-md-4">
                         <h1 className="mainTitle">Hello I'm Débora.</h1>
                         <h5 className="subTitle">I'm a web developer who loves code, design and build things</h5>
                     </div>
@@ -33,21 +45,39 @@ export const Home = () => {
                 </div>
 
                 <div class="row">
-                    <div class="col-md-9">
-                    <button className="button">
-                            <span> Contact
-                            </span>
+                    <div className="col-md-9">
+                        <button onClick={handleClickOpen} className="button">
+                                Contact
                         </button>
                     </div>
-                    <div class="col-md-3 mt-4">
-                    <button className="button">
-                            <span> Projects
-                            </span>
+                    { popUp ?
+                    <div className="main">
+                        <div className="popup">
+                                <Contact/>
+                                <h1 onClick={closePopUp}>x</h1>
+                        </div>
+                    </div> : ""
+                    }
+
+                <div class="row">
+                    <div className="col-md-10"></div>
+                    <div class="col-md-2 mt-4">
+                        <button className="button">
+                                Projects
                         </button>
                     </div>
                 </div>
-            </div>
+            </div> 
         </div>
+
+{/* aquí aparecerían las secciones al hacer click en el menú */}
+        <div className="containerSections"> 
+        <section>1</section>
+        <section>2</section>
+        <section>3</section>
+        </div>
+
+    </div>
     )
 }
 
